ef94cf77fb7bd3fbaf3fe01d5d305b3636084c1f
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-02 12:41:40 -0500

    created model for User

---
+++ routes/users/model/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    username: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    postHistory: [{ type: mongoose.Schema.ObjectId, ref: 'post' }],
+    commentHistory: [{ type: mongoose.Schema.ObjectId, ref: 'comment' }],
+}, { timestamps: true });
+
+module.exports = mongoose.model("user", userSchema);
