8a2fcdd390e271d3e5a23dbd7ad814320b547b9c
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-07 23:12:39 -0500

    Set up comments controller and router

--- routes/comments/commentsRouter.js
+++ routes/comments/commentsRouter.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const { jwtMiddleware, checkIsEmpty } = require('../users/lib/authMiddleware/index');
+const { createComment, getComments, deleteComment, updateComment } = require('./controller/commentsController');
 
 router.get('/', function(req, res, next) {
   res.send('Hello from the comments route');
 });
 
 // Create a comment
-router.post('/create-comment',);
+router.post('/create-comment', jwtMiddleware, checkIsEmpty, createComment);
 
 // Get all commemts from a user
-router.get('/get-all-comments/',);
+router.get('/get-all-comments/', jwtMiddleware, getComments);
 
 // Delete a comment
-router.delete('/delete-comment/:id',);
+router.delete('/delete-comment/:id', jwtMiddleware, deleteComment);
 
 // Update a comment
-router.put('/update-comment/:id',);
+router.put('/update-comment/:id', jwtMiddleware, checkIsEmpty, updateComment);
 
 module.exports = router;
--- routes/comments/controller/commentsController.js
+++ routes/comments/controller/commentsController.js
@@ -3,6 +3,38 @@ const Posts = require('../../posts/model/Posts');
 const User = require('../../users/model/User');
 const { errorHandler } = require('../../users/utils/errorHandler');
 
+const createComment = async (req, res) => {
+    try {
+    } catch (error) {
+        res.status(500).json(errorHandler(error))
+    }
+}
+
+const getComments = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ message: 'Error', error: error.message });
+    }
+};
+
+const updateComment = async (req, res) => {
+    try {
+    } catch (error) {
+      res.status(500).json({ message: error, error: errorHandler(error) });
+    }
+};
+
+const deleteComment = async (req, res) => {
+    try {
+    } catch (error) {
+      res.status(500).json({ message: "error", error: error });
+    }
+};
+
 module.exports = {
-	
+	createComment,
+	getComments,
+	updateComment,
+	deleteComment,
 }
